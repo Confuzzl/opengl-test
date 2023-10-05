60bcc40ff3427f988ecd422f55402f188d374b63
Confuzzl <fuzzltheduzzl@gmail.com>
2023-10-04 21:52:25 -0400

    fixed merge conflict

--- src/util/gl_utils.cpp
+++ src/util/gl_utils.cpp
@@ -1,19 +1,1 @@
-<<<<<<< HEAD
 #include "gl_utils.h"
-=======
-#include "gl_utils.h"
-FailedShaderCompilationException::FailedShaderCompilationException(
-    const char *message)
-    : std::runtime_error(message) {}
-FailedTextureLoadException::FailedTextureLoadException(const char *message)
-    : std::runtime_error(message) {}
-FailedWindowCreationException::FailedWindowCreationException(
-    const char *message)
-    : std::runtime_error(message) {}
-UnallocatedGLObjectUsageException::UnallocatedGLObjectUsageException(
-    const char *message)
-    : std::runtime_error(message) {}
-InvalidFaceTextureListException::InvalidFaceTextureListException(
-    const char *message)
-    : std::runtime_error(message) {}
->>>>>>> 62d31c1e128095c58d893cd591ff692249d10ad0
--- src/util/gl_utils.h
+++ src/util/gl_utils.h
@@ -1,24 +1,4 @@
 #pragma once
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-<<<<<<< HEAD
 #include <glad/glad.h>
-=======
-#include <glad/glad.h>
-#include <stdexcept>
-struct FailedShaderCompilationException : public std::runtime_error {
-  FailedShaderCompilationException(const char *message);
-};
-struct FailedTextureLoadException : public std::runtime_error {
-  FailedTextureLoadException(const char *message);
-};
-struct FailedWindowCreationException : public std::runtime_error {
-  FailedWindowCreationException(const char *message);
-};
-struct UnallocatedGLObjectUsageException : public std::runtime_error {
-  UnallocatedGLObjectUsageException(const char *message);
-};
-struct InvalidFaceTextureListException : public std::runtime_error {
-  InvalidFaceTextureListException(const char *message);
-};
->>>>>>> 62d31c1e128095c58d893cd591ff692249d10ad0
