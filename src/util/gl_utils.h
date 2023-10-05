f0e60caedb1dd028ea85e8adb8480cd35b140609
Confuzzl <fuzzltheduzzl@gmail.com>
2023-10-04 21:48:38 -0400

    override gl_utils

--- src/util/gl_utils.h
+++ src/util/gl_utils.h
@@ -2,16 +2,3 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
-#include <stdexcept>
-struct FailedShaderCompilationException : public std::runtime_error {
-  FailedShaderCompilationException(const char *message);
-};
-struct FailedTextureLoadException : public std::runtime_error {
-  FailedTextureLoadException(const char *message);
-};
-struct FailedWindowCreationException : public std::runtime_error {
-  FailedWindowCreationException(const char *message);
-};
-struct UnallocatedGLObjectUsageException : public std::runtime_error {
-  UnallocatedGLObjectUsageException(const char *message);
-};
